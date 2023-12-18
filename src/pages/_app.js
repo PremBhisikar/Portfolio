15637ebac37d0345b19eed51db776b4960c7f97f
Prem <prembhisikar151996@gmail.com>
2023-12-18 23:37:50 +0530

    home page design

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from '@/Components/Footer';
 import NavBar from '@/Components/NavBar';
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google';
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }) {
         <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
           <NavBar/>
           <Component {...pageProps} />
+          <Footer/>
         </main>
       
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,14 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-mont)', ...fontFamily.sans],
       },
-      color: {
+      colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
+      },
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',
       }
     },
   },
