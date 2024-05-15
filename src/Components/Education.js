99f09aa779f3ba1b2c89b9d8815e12e834c49c77
Prem <prembhisikar151996@gmail.com>
2024-05-15 13:25:55 +0530

    Favicon,about page changes

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -40,7 +40,7 @@ export const Education = () => {
     return (
         <div className='my-64'>
             <h2 className='font-bold text-8xl mb-32 w-full text-center'>
-                Experience
+                Education
             </h2>
             <div ref={ref} className='w-[75%] mx-auto relative'>
                 <motion.div
